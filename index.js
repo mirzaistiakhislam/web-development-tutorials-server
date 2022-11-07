8bcb310cc4895bbafab05e3e0b07a1267cd03d15
mirzaistiakhislam <mirza15-9603@diu.edu.bd>
2022-11-07 22:40:43 +0600

    updated courses part

--- index.js
+++ index.js
@@ -19,20 +19,24 @@ app.get('/course-categories', (req, res) => {
 
 app.get('/category/:id', (req, res) => {
     const id = req.params.id;
-    const categoryCourses = courses.filter(course => course.category_id === id);
-    res.send(categoryCourses);
-})
-
-app.get('/courses/:id', (req, res) => {
-    const id = req.params.id;
     if (id === '07') {
         res.send(courses);
     }
     else {
-        const selectedCourses = courses.find(course => course._id === id);
-        res.send(selectedCourses);
+        const categoryCourses = courses.filter(course => course.category_id === id);
+        res.send(categoryCourses);
     }
 
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourses = courses.find(course => course._id === id);
+    res.send(selectedCourses);
 })
 
 app.listen(port, () => {
