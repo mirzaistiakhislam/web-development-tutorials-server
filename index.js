7e3cf4e8ad8b502936c4637b331496e157bbb7fd
mirzaistiakhislam <mirza15-9603@diu.edu.bd>
2022-11-07 20:02:27 +0600

    update category id & name

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+
+app.use(cors());
+
 const categories = require('./data/categories.json');
 
 app.get('/', (req, res) => {
